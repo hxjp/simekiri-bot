0ab1bcebc88c5d7c4e863c3879265cfd8e74de16
Shumpei Shiraishi <shumpei.shiraishi@gmail.com>
2017-08-03 12:11:41 +0900

    add忘れ

---
+++ ts/messages-ja.ts
@@ -0,0 +1,18 @@
+export const messages = {
+  wrongCommand: 'コマンド間違ってるわよ！バカ！💢',
+  deadlineAdded: '〆切追加しといたわよ😊',
+  deadlineUpdated: '〆切更新しといたわ。〆切の数が増えもしないのつまんない…！😒',
+  deadlineCanceled: 'ちょっと…！なんで〆切外してるのよ…！😥',
+  wrongDeadlineId: 'そんなIDの〆切ないんだけど💢',
+  onTaskDone: '記事書き終わったの？ふん、ひとまずお疲れ様と言っておくわ😉',
+  availableCommands: '今のあなたが使えるコマンドはこんなところね。早く〆切増やしなさいよ😠',
+  deadlineListFew: '登録されてる〆切が少ないわ…不満😡',
+  deadlineListNormal: '登録されてる〆切は今これだけね。',
+  deadlineListMany: '〆切がいっぱい！😆',
+  deadlineListEmpty: '〆切が1個も登録されてない…仕事してるの？😡',
+  remindMessageToday: '〆切今日だけど、当然書けてるわよね？😏あとは仕上げってところかしら？',
+  remindMessageBefore1: '〆切%s前よ。執筆進んでる？😘',
+  remindMessageBefore2: '〆切%s前よ。あとちょっとだけど、きっと順調よねえ？😉',
+  remindMessageAfter1: '〆切%s過ぎてるんだけど😠状況は？',
+  remindMessageAfter2: '〆切%s過ぎてるんだけど…💢まさかこのまま逃げ切ろうなんてしてない…わよね…？',
+};
